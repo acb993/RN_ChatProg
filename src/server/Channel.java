62615a95fcd87e70d73348541f604f22a2bd6239
acc044 <michel.kapell@haw-hamburg.de>
2017-11-09 15:35:09 +0100

    Klassen Channel und ClientConnection bearbeitet.

--- src/server/Channel.java
+++ src/server/Channel.java
@@ -1,4 +1,30 @@
 package server;
 
-public class Channel {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Channel extends Thread {
+
+    private List<ClientConnection> user;
+    private int channelID;
+    private String channelName;
+    private Queue<String> messages;
+
+
+
+    public Channel(int ID,String ChannelName)
+    {
+        this.channelID=ID;
+        this.channelName=ChannelName;
+        user = new ArrayList<>();
+        messages= new LinkedList<>();
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
 }
--- src/server/ClientConnection.java
+++ src/server/ClientConnection.java
@@ -1,4 +1,26 @@
 package server;
 
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ClientConnection {
+
+    private ServerSocket socket;
+    private int id;
+    private String username;
+
+
+    public ClientConnection(int id, int port) throws IOException {
+        this.id=id;
+        ServerSocketFactory socketFactory = SSLServerSocketFactory.getDefault();
+        socket = socketFactory.createServerSocket(port);
+    }
+
+    public void run()
+    {
+
+    }
 }
