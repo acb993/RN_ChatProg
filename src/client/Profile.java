adbb59649b1b5ff42d7e51c358121148e8c72ba6
Anastasios Palatiou <anastasios.palatiou@haw-hamburg.de>
2017-11-14 10:55:02 +0100

    Ordner Struktur geändert

---
+++ src/client/Profile.java
@@ -0,0 +1,14 @@
+package client;
+
+public class Profile {
+
+    private String firstName;
+    private String lastName;
+    private String eMail;
+
+    public Profile(String firstName,String lastName, String eMail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+    }
+}
