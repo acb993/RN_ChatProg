7650f0091e1255da8ec8041e7a6299e04a4c6ecd
acc044 <michel.kapell@haw-hamburg.de>
2017-11-16 15:47:36 +0100

    Angefangen mit der GUI

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,32 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ButtonSend;
+
+    @FXML
+    void sendMessage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert ButtonSend != null : "fx:id=\"ButtonSend\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
+
